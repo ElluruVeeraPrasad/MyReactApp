d93c25b13b3f0078b82df5148b777bd0302f6cc0
Elluru Veera Prasad <144753371+ElluruVeeraPrasad@users.noreply.github.com>
2026-06-03 13:04:00 +0530

    Change login header text to 'Login to my world'

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -54,7 +54,7 @@ function Login() {
 
       <div className="login-box">
 
-        <h2>{isRegister ? "Register" : "Login to our application"}</h2>
+        <h2>{isRegister ? "Register" : "Login to my world"}</h2>
 
         <form onSubmit={handleSubmit}>
 
